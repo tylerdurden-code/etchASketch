34f4da97100181d1f0794bb527266f52485ed021
Tyler Durden <tylerdurden1412@gmail.com>
2022-02-21 15:46:49 +0200

    finish mouse over event

--- javascript.js
+++ javascript.js
@@ -8,15 +8,29 @@ function divArray(num) { // makes a num length array with div iterations inside 
 }
 
 
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 
 var divs = divArray(256);
 
 for (i = 0; i < divs.length; i++) {
     var g = document.createElement('div')
-    g.setAttribute('id',divs[i])
+    //g.setAttribute('id',divs[i])
     g.setAttribute('class', "same")
     container.appendChild(g);
-    g.innerHTML = `${i}`;
+    // g.innerHTML = `${i}`;
 }
+const same = document.querySelectorAll('.same')
+for (i =0; i < same.length; i++) {
+    same[i].addEventListener('mouseover',function(e) {
+        console.log("PAPA")
+        e.target.style.backgroundColor = 'black';
+    })
+}
+
+
+
+
+
+
+
 
